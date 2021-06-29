8ceb8ac1ccb1c2c0d9765b2772c210dc9f51646e
thais-souza <82286686+thais-souza@users.noreply.github.com>
2021-06-28 22:04:10 -0300

    Delete fxyt.c

--- fxyt.c
+++
@@ -1,8 +0,0 @@
-//======================================================================================
-// SUBROTINA PARA A DETERMINAÇÃO DO TERMO FONTE
-//======================================================================================
-double FXYT ( int npf , double *pf , double x , double y , double t )
-{
-  return ( 0.0 ) ;
-  //return ( exp ( -t ) * ( pf [ 0 ] * sin ( pi * x ) + pf [ 1 ] * sin ( pi * y ) ) ) ;
-}
